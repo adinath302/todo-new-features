8698b7d1c26ed534cae4fcfedb621ba000069cc3
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-06-11 23:06:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+import ProfileCards from './components/ProfileCards'
+import profileimges from "./assets/profile.jpg"
+import profile2 from "./assets/profile2.jpg"
+import Button from "./components/Button.jsx"
+import Card from './components/Cards.jsx'
+import Card1 from './components/Card1.jsx'
+import Inputsection from './components/Inputsection.jsx'
+import Impression from './components/impression.jsx'
+import Cardsection1 from './components/Cardsection1.jsx'
+import Teamsection from './components/Teamsection.jsx'
+import Testimonials from './components/Testimonials.jsx'
+import Login from './components/login/Login.jsx'
+import Todo from './components/Todo/Todo.jsx'
+
+function App() {
+
+  return (
+    <>
+      {/* <Login /> */}
+      {/* <Inputsection /> */}
+      {/* <Impression /> */}
+      {/* <Cardsection1 /> */}
+      {/* <Teamsection /> */}
+      {/* <Testimonials /> */}
+      <Todo />
+    </>
+  )
+}
+
+export default App;
